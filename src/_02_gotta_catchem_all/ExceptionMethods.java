033818a548132375f7165a2ec3ad6654cfcd64e6
zhaoamy <zhaoamy@localhost>
2021-01-23 17:48:30 -0800

    -_-

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -2,4 +2,23 @@ package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
 	
+	public static double divide(double d1, double d2) {
+		double quotient = d1/d2;
+		if(d2 == 0.0) {
+			throw new IllegalArgumentException();
+		}
+		return quotient;
+	}
+	
+	public static String reverseString(String string) {
+		if(string.isEmpty()) {
+			throw new IllegalStateException();
+		}
+		
+		String s = "";
+		for(int i = string.length()-1; i>=0; i++) {
+			s = s + string.charAt(i);
+		}
+;		return s;
+	}
 }
